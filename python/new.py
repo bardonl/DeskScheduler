c2b22ab3aa40851bc1fdb5c4a5c7e049e9b88696
bardonl <bart.degeus@outlook.com>
2022-06-05 15:26:24 +0200

    [BUGFIX] Fix bug where 00:00 until 09:59 was bugged with an extra leading 0 This bug caused python not to understand what to do with times like 00:30:00

--- python/new.py
+++ python/new.py
@@ -38,7 +38,8 @@ cursor.execute(query,(day,time_now,))
 data = cursor.fetchall()
 
 if data:
-    print(data)
+    print(str(day))
+    print(str(time_now))
     t_end = time.time() + 9
     for row in data:
       if(str(row["start_time"]) <= str(time_now) and str(row["end_time"]) >= str(time_now) and row["movement"] == 0):
